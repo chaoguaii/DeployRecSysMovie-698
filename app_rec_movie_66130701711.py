393f28f6f8576dca0b4e3b86d3b84412e426fa41
chaoguaii <126164848+chaoguaii@users.noreply.github.com>
2024-10-27 16:27:12 +0700

    Update app_rec_movie_66130701711.py

--- app_rec_movie_66130701711.py
+++ app_rec_movie_66130701711.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pickle
 import pandas as pd
@@ -10,26 +9,27 @@ with open('66130701711_recommendation_movie_svd.pkl', 'rb') as file:
 
 # Streamlit app
 st.title("Movie Recommendation System")
-st.write("Get personalized movie recommendation based on your user ID!")
+st.write("Get personalized movie recommendations based on your user ID!")
 
 # User input
 user_id = st.number_input("Enter your user ID:", min_value=1, step=1)
 
 # Recommendation generation
 if st.button("Get Recommendations"):
-
-# Filter out movies the user has already rated
-rated_user_movies = movie_ratings[movie_ratings['userId'] == user_id]['movieId'].values
-unrated_movies = movies[~movies['movieId'].isin(rated_user_movies)]['movieId']
-pred_rating = [svd_model.predict(user_id, movie_id) for movie_id in unrated_movies]
+    # Filter out movies the user has already rated
+    rated_user_movies = movie_ratings[movie_ratings['userId'] == user_id]['movieId'].values
+    unrated_movies = movies[~movies['movieId'].isin(rated_user_movies)]['movieId']
+    
+    # Predict ratings for unrated movies
+    pred_rating = [svd_model.predict(user_id, movie_id) for movie_id in unrated_movies]
 
     # Sort predictions by estimated rating in descending order
     sorted_predictions = sorted(pred_rating, key=lambda x: x.est, reverse=True)
 
-    # Get top 10 movie recommendation
+    # Get top 10 movie recommendations
     top_recommendations = sorted_predictions[:10]
 
-    # Display top recommendation
+    # Display top recommendations
     st.write(f"\nTop 10 movie recommendations for User {user_id}:")
     for recommendation in top_recommendations:
         movie_title = movies[movies['movieId'] == recommendation.iid]['title'].values[0]
